9dbfc8e38d96bec7b1e4c480a4eb4f5ce9450242
Grey Boolean <greyboolean@gmail.com>
2024-05-01 18:30:51 +0530

    add auth guard and set up Roles decorator and Public decorator

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
+import { AuthGuard } from './guards/auth.guard';
+import { RolesGuard } from './guards/roles.guard';
 
 @Module({
+  imports: [UsersModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
 })
 export class AuthModule {}
--- server/src/auth/guards/auth.guard.ts
+++ server/src/auth/guards/auth.guard.ts
@@ -1,28 +1,43 @@
-import clerkClient from '@clerk/clerk-sdk-node';
 import {
   CanActivate,
   ExecutionContext,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Request } from 'express';
+import clerkClient from '@clerk/clerk-sdk-node';
+import { LocalUsersService } from 'src/users/local-users.service';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
+  constructor(
+    private reflector: Reflector,
+    private localUserService: LocalUsersService,
+  ) {}
 
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
     const token =
       this.extractTokenFromCookie(request) ||
       this.extractTokenFromHeader(request);
-
     if (!token) {
       throw new UnauthorizedException('User is not authenticated');
     }
 
     try {
       const payload = await clerkClient.verifyToken(request.cookies.__session);
-      const user = await clerkClient.users.getUser(payload.sub);
+      const user = await this.localUserService.findOneByClerkId(payload.sub);
+      request['user'] = user;
     } catch (err) {
       throw new UnauthorizedException('User is not authenticated');
     }
